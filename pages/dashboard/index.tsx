a3dc4cf62b2caf98413c9b9b7d7a11e4967302b9
Jaycex85 <cruits@gmail.com>
2026-03-02 16:31:29 +0100

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 useEffect(() => {
   if (!user) return;
-  if (user.role === "super_admin") router.push("/dashboard/admin/clubs");
+  if (user.role === "admin") router.push("/dashboard/admin/clubs");
   else if (user.role === "responsable") router.push("/dashboard/responsable/equipes");
 }, [user]);
