e341c3b273e5443fa79365aa163ab862b8f51b02
Jaycex85 <cruits@gmail.com>
2026-03-02 11:54:24 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-// pages/index.tsx
 import { GetServerSideProps } from 'next'
-
-export default function Home() {
-  return null // rien à afficher, la redirection se fait côté serveur
-}
 
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
     redirect: {
       destination: '/auth',
-      permanent: false, // non permanent, pour pouvoir changer la destination plus tard
-    },
+      permanent: false
+    }
   }
+}
+
+export default function Home() {
+  return null
 }
