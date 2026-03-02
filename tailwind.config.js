f359db6c0f4a63d4f53dc6fa2e789c81b1482d8b
Jaycex85 <cruits@gmail.com>
2026-03-02 12:35:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001f3f',       // couleur custom
+        graylight: '#f5f5f5',  // couleur custom
+      },
+    },
+  },
   plugins: [],
-  darkMode: 'class', // active le mode sombre
 }
- 
