10a28f786011acd31b5601405ce4bf707e9c3362
Jaycex85 <cruits@gmail.com>
2026-03-02 10:34:49 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,17 @@
+// pages/index.tsx
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/auth') // redirige vers page login/signup
+  }, [router])
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-gray-100">
+      <p>Redirection...</p>
+    </div>
+  )
+}
