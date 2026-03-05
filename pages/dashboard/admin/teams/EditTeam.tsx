2f5511866a708f69eecd8497988bf9c2417fabe6
Jaycex85 <cruits@gmail.com>
2026-03-05 10:24:27 +0100

    Update EditTeam.tsx

--- pages/dashboard/admin/teams/EditTeam.tsx
+++ pages/dashboard/admin/teams/EditTeam.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { supabase } from '../../../utils/supabaseClient'
+import { supabase } from '../../../../utils/supabaseClient'
 
 type TeamFormProps = {
   teamId: string
